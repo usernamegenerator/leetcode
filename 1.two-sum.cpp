d1f599ddd6a21718d672141f43855c74725f6640
bigship <bigship1990@gmail.com>
2019-10-02 21:51:28 -0600

    two sum 2nd solution

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -4,17 +4,21 @@
  * [1] Two Sum
  */
 #include <vector>
+#include <map>
+
 using namespace std;
 // @lc code=start
-/*
+
+class Solution
+{
+public:
+    /*
 brute force solution:
 iterate the array from the first index
     iterate the array from next index
         check if they add up to the targe
 */
-class Solution
-{
-public:
+    /*
     vector<int> twoSum(vector<int> &nums, int target)
     {
         vector<int> res;
@@ -36,5 +40,36 @@ public:
         }
         return res;
     }
+*/
+    // use hashmap look up solution:
+    /*
+iterate through the array, push the elements to map, <key,value> = <element, index>
+iterate through the array again, take target-element, and look up in the map
+if found, return results
+*/
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> m;
+        vector<int> res;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
+        {
+            m.insert(pair<int, int>(*it, it - nums.begin()));
+        }
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
+        {
+            int remain = target - *it;
+            map<int, int>::iterator ij = m.find(remain);
+            // make sure it doesn't count itself
+            // e.g. [3,2,4] 6, will not return [0,0] which is 3+3
+            if (ij != m.end() && (ij->second != it-nums.begin()))
+            {
+                res.push_back(it - nums.begin());
+                res.push_back(ij->second);
+                return res;
+            }
+        }
+        return res;
+    }
 };
+
 // @lc code=end
