ac35f871bd1060bdff0b329fd5b14786e21364d7
bigship <bigship1990@gmail.com>
2019-10-03 21:49:55 -0600

    Update 3.longest-substring-without-repeating-characters.cpp

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -5,6 +5,7 @@
  */
 #include <string>
 #include <map>
+#include <set>
 #include <iostream>
 using namespace std;
 
@@ -13,11 +14,9 @@ class Solution
 {
 public:
     /*
-brute force:
-iterater through the string, start with the first
-check if following have repeated.
-store the res
-*/
+    first slide window solutions:
+    */
+    /*
     int lengthOfLongestSubstring(string s)
     {
         if(s.empty())
@@ -49,6 +48,51 @@ store the res
             }
         }
         return maxString.size();
+    }
+*/
+
+    // slide window solution with less code
+    /* 
+     p  w  w  k  e  w
+     ij                         not find repeat, set add p, j++, update window size 1
+     i  j                       not find repeat, set add w, j++, update window size 2
+     i     j                    find repeat, delete p and move to next i
+        i  j                    find repeat, delete w and move to next i
+           ij                   no repeat, set add w (already there), j++, no need to update window size
+           i  j                 no repeat, set add k, j++, no need to update window size
+           i     j              no repeat, set add e, j++, update window size to 3
+           i        j           find repeat, delete w and move to next i
+              i        j        j out of bound, exit
+                                return 3;
+    */
+    int lengthOfLongestSubstring(string s)
+    {
+        int i = 0;
+        int j = 0;
+        int max = 0;
+        set<char> set;
+        while (i < s.size() && j < s.size())
+        {
+            // find a repeat, this i ends
+            if (set.find(s[j]) != set.end())
+            {
+                set.erase(s[i]);
+                i++;
+            }
+            else // not find a repeat
+            {
+                set.insert(s[j]);
+                j++;
+
+                // update current max window size
+                // j - i is the window
+                if (j - i > max)
+                {
+                    max = j - i;
+                }
+            }
+        }
+        return max;
     }
 };
 // @lc code=end
