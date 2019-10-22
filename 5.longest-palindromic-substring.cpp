fccce219685df65f965b6e8cfe72faf35ff30f15
bigship <bigship1990@gmail.com>
2019-10-21 23:09:46 -0600

    Update 5.longest-palindromic-substring.cpp

--- 5.longest-palindromic-substring.cpp
+++ 5.longest-palindromic-substring.cpp
@@ -65,48 +65,119 @@ Time Limit Exceeded
     /****************************************************/
     // method 2:
     // expland from center
+    // filling # for abbc type (even centered)
+    /*    string longestPalindrome(string s)
+    {
+        int size = s.size();
+        string news;
+        int i = 0;
+        while (i < size)
+        {
+            news.append("#");
+            news.append(1, s[i++]);
+        }
+        news.append("#");
+
+        // cout << news << endl;
+
+        int new_size = news.size();
+        string maxWithPadding = "";
+        for (i = 0; i < new_size; i++)
+        {
+            string localMaxWithPadding = getPalindrome(news, i);
+            if (localMaxWithPadding.size() > maxWithPadding.size())
+            {
+                maxWithPadding = localMaxWithPadding;
+            }
+        }
+        string res;
+        for(i=0;i<maxWithPadding.size();i++)
+        {
+            if(maxWithPadding[i]=='#')
+            {
+                continue;
+            }
+            res.append(1,maxWithPadding[i]);
+        }
+        return res;
+    }
+
+    string getPalindrome(string s, int i)
+    {
+        int j = 0;
+        while (i - j >= 0 && i + j < s.size())
+        {
+            if (s[i - j] != s[i + j])
+            {
+                break;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        j--;
+        // cout << i - j << " " << j + j << endl;
+        // cout << s.substr(i - j, j + j + 1)<<endl;
+        return s.substr(i - j, j + j + 1);
+    } */
+
+    /***********************************************************/
+    // method 3:
+    // dynamic programming
+    // https://www.youtube.com/watch?v=m2Mk9JN5T4A
     string longestPalindrome(string s)
     {
         int size = s.size();
-        string maxString = "";
-        // i is the center
+        // 1. one letter, return
+        if (size == 1 || size == 0)
+        {
+            return s;
+        }
+        /*         // 2. two letters, if same, return
+        if (size == 2)
+        {
+            if (s[0] == s[1])
+            {
+                return s;
+            }
+            else
+            {
+                return s.substr(0, 1);
+            }
+        } */
+
+        // 3. dynamic programming
+        bool **isPalindrome = new bool *[size];
         for (int i = 0; i < size; i++)
         {
-            if (maxString.size() == 0)
+            isPalindrome[i] = new bool[size];
+        }
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
             {
-                maxString = s[i];
-            }
-
-            int j = i - 1;
-            int k = i + 1;
-            while (s[j] == s[i] && j >= 0)
-            {
-                string temp = s.substr(j, i - j + 1);
-                if (temp.size() > maxString.size())
-                    maxString = temp;
-                j--;
-            }
-            while (s[k] == s[i] && k < size)
-            {
-                string temp = s.substr(i, k - i + 1);
-                if (temp.size() > maxString.size())
-                    maxString = temp;
-                k++;
-            }
-            while (j >= 0 && k < size)
-            {
-                if (s[j] != s[k])
-                    break;
-                string temp = s.substr(j, k - j + 1);
-                if (temp.size() > maxString.size())
-                {
-                    maxString = temp;
-                }
-                j--;
-                k++;
+                isPalindrome[i][j] = false;
             }
         }
-        return maxString;
+        string res = "";
+        for (int i = 0; i < size; i++)
+        {
+            isPalindrome[i][i] = true;
+            for (int j = 0; j <= i; j++)
+            {
+                // i - j <= 2 is to prevent the overflow of [i-1] [j+1]
+                isPalindrome[i][j] = s[i] == s[j] && (i - j <= 2 || isPalindrome[i - 1][j + 1]) ;
+                if (isPalindrome[i][j])
+                {
+                    if (i - j + 1 > res.size())
+                    {
+                        res = s.substr(j, i - j + 1);
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
 
