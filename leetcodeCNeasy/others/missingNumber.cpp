7204b09bd7bb94599ba133d1e0db2caaad6b0595
bigship <bigship1990@gmail.com>
2019-11-10 14:10:52 -0700

    Create missingNumber.cpp

---
+++ leetcodeCNeasy/others/missingNumber.cpp
@@ -0,0 +1,38 @@
+// https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/26/others/69/
+
+/*
+缺失数字
+给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+
+示例 1:
+
+输入: [3,0,1]
+输出: 2
+示例 2:
+
+输入: [9,6,4,2,3,5,7,0,1]
+输出: 8
+说明:
+你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+*/
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        vector<bool> inorder(nums.size() + 1, false);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            inorder[nums[i]] = true;
+        }
+        for (int i = 0; i < inorder.size(); i++)
+        {
+            if (inorder[i] == false)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
