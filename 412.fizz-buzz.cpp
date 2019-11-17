a230510482bc5bd6d1ed2de0c3ca35f6a3d8da08
bigship <bigship1990@gmail.com>
2019-11-17 16:14:07 -0700

    [412] Fizz Buzz

---
+++ 412.fizz-buzz.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=412 lang=cpp
+ *
+ * [412] Fizz Buzz
+ *
+ * https://leetcode.com/problems/fizz-buzz/description/
+ *
+ * algorithms
+ * Easy (60.16%)
+ * Likes:    627
+ * Dislikes: 926
+ * Total Accepted:    253.3K
+ * Total Submissions: 419.3K
+ * Testcase Example:  '1'
+ *
+ * Write a program that outputs the string representation of numbers from 1 to
+ * n.
+ * 
+ * But for multiples of three it should output “Fizz” instead of the number and
+ * for the multiples of five output “Buzz”. For numbers which are multiples of
+ * both three and five output “FizzBuzz”.
+ * 
+ * Example:
+ * 
+ * n = 15,
+ * 
+ * Return:
+ * [
+ * ⁠   "1",
+ * ⁠   "2",
+ * ⁠   "Fizz",
+ * ⁠   "4",
+ * ⁠   "Buzz",
+ * ⁠   "Fizz",
+ * ⁠   "7",
+ * ⁠   "8",
+ * ⁠   "Fizz",
+ * ⁠   "Buzz",
+ * ⁠   "11",
+ * ⁠   "Fizz",
+ * ⁠   "13",
+ * ⁠   "14",
+ * ⁠   "FizzBuzz"
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<string> fizzBuzz(int n)
+    {
+        vector<string> res;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                res.push_back("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                res.push_back("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                res.push_back("Buzz");
+            }
+            else
+            {
+                res.push_back(to_string(i));
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
