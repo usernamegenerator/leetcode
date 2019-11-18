f4af1d221af190b3c45a9341c922c28aebfc00ea
bigship <bigship1990@gmail.com>
2019-11-17 22:22:41 -0700

    [20] Valid Parentheses

---
+++ 20.valid-parentheses.cpp
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (37.32%)
+ * Likes:    3620
+ * Dislikes: 175
+ * Total Accepted:    756.5K
+ * Total Submissions: 2M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        if (s.empty())
+            return true;
+        if ((s.size() % 2) == 1)
+            return false;
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.empty())
+                    return false;
+                if (s[i] == ')' && st.top() != '(')
+                {
+                    return false;
+                }
+
+                if (s[i] == ']' && st.top() != '[')
+                {
+                    return false;
+                }
+
+                if (s[i] == '}' && st.top() != '{')
+                {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        if (!st.empty())
+        {
+            return false;
+        }
+        return true;
+    }
+};
+// @lc code=end
