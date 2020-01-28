eca9ab0d0aa60a934f13d2a426bdea71efaacf3a
usernamegenerator <bigship1990@gmail.com>
2020-01-27 22:10:56 -0700

    Update longestPalindrome.cpp

--- leetcodeCNmedium/arrayAndstring/longestPalindrome.cpp
+++ leetcodeCNmedium/arrayAndstring/longestPalindrome.cpp
@@ -13,9 +13,49 @@
 输出: "bb"
 */
 
-class Solution {
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
 public:
-    string longestPalindrome(string s) {
-        
+    // method 1:
+    // iterate through the array, use the index to expand
+    string longestPalindrome(string s)
+    {
+        string res;
+        for (int i = 0; i < s.size(); i++)
+        {
+            //1 center
+            string s1 = centerExpand(s, i, i);
+            //2 center
+            string s2 = centerExpand(s, i, i + 1);
+            if (s1.size() > res.size())
+            {
+                res = s1;
+            }
+            if (s2.size() > res.size())
+            {
+                res = s2;
+            }
+        }
+        return res;
+    }
+    string centerExpand(string s, int leftCenter, int rightCenter)
+    {
+        int left = leftCenter;
+        int right = rightCenter;
+        while (left >= 0 && right < s.size() && s[left] == s[right])
+        {
+            left--;
+            right++;
+        }
+        // need to compensate
+        left++;
+        right--;
+        return s.substr(left, right - left + 1);
     }
 };
