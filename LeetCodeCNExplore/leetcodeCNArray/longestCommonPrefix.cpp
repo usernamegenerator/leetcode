5477f44b820d02d4506241c743f055f851ea4625
usernamegenerator <bigship1990@gmail.com>
2020-07-11 16:32:59 -0600

    Create longestCommonPrefix.cpp

---
+++ LeetCodeCNExplore/leetcodeCNArray/longestCommonPrefix.cpp
@@ -0,0 +1,57 @@
+/*
+最长公共前缀
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+
+所有输入只包含小写字母 a-z 。
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string res;
+        if (strs.size() == 0)
+        {
+            return res;
+        }
+        for (int i = 0; i < strs[0].size(); i++)
+        {
+            int s = 1;
+            while (s < strs.size())
+            {
+                if (i >= strs[s].size())
+                    return res;
+                if (strs[s][i] == strs[0][i])
+                {
+                    s++;
+                }
+                else
+                {
+                    return res;
+                }
+            }
+            if (s == strs.size())
+            {
+                res.push_back(strs[0][i]);
+            }
+        }
+        return res;
+    }
+};
