bb9ae69c1b1208774aaf76812029a126a8df836a
usernamegenerator <bigship1990@gmail.com>
2020-07-23 15:07:31 -0600

    Create removeElements.cpp

---
+++ LeetCodeCNExplore/leetcodeCNLinkedList/removeElements.cpp
@@ -0,0 +1,46 @@
+/*
+移除链表元素
+删除链表中等于给定值 val 的所有节点。
+
+示例:
+
+输入: 1->2->6->3->4->5->6, val = 6
+输出: 1->2->3->4->5
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        if (head == nullptr)
+        {
+            return nullptr;
+        }
+        while (head && head->val == val)
+        {
+            head = head->next;
+        }
+        ListNode *cur = head;
+        while (cur && cur->next)
+        {
+            if (cur->next->val == val)
+            {
+                cur->next = cur->next->next;
+            }
+            else
+            {
+                cur = cur->next;
+            }
+        }
+        return head;
+    }
+};
